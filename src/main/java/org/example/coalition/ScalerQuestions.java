5e215a39fcce9345aebfbe7b93b1eb58e7250d69
Yathin Somaiah <yathin51@users.noreply.github.com>
2021-05-26 00:53:34 +0530

    Scaler Questions

--- src/main/java/org/example/coalition/ScalerQuestions.java
+++ src/main/java/org/example/coalition/ScalerQuestions.java
@@ -5,49 +5,50 @@ import org.slf4j.LoggerFactory;
 
 public class ScalerQuestions {
 
-    private Logger log = LoggerFactory.getLogger(ScalerQuestions.class);
-    void function(){
-        int i  =0 ;
-        int sum =0;
-        while(i < 100){
+    private static Logger log = LoggerFactory.getLogger(ScalerQuestions.class);
+
+    void function() {
+        int i = 0;
+        int sum = 0;
+        while (i < 100) {
             sum = sum + i;
-            sum = i+ sum;
-            i+= 1;
+            sum = i + sum;
+            i += 1;
         }
-        log.info("{}",sum);
+        log.info("{}", sum);
     }
 
     public static void main(String[] args) {
         ScalerQuestions scalerQuestions = new ScalerQuestions();
         scalerQuestions.function();
-        System.out.println(fun(100,2000));
-        System.out.println(scalerQuestions.fib(6));
+        log.info("{}", fun(100, 2000));
+        log.info("{}", scalerQuestions.fib(6));
 
-        for(int i =0; i< 6; i++){
-            System.out.println(printTribRec(i));
+        for (int i = 0; i < 6; i++) {
+            log.info("", printTribRec(i));
         }
 
     }
 
-    int fib( int n){
-        if(n<=1)
+    int fib(int n) {
+        if (n <= 1)
             return n;
-        return fib(n-1) + fib(n-2);
+        return fib(n - 1) + fib(n - 2);
 
     }
 
-    static  int fun(int a, int b){
-        if(b==0)
+    static int fun(int a, int b) {
+        if (b == 0)
             return a;
-        else return fun(b, a%b);
+        else return fun(b, a % b);
     }
 
-    static int printTribRec(int n){
-        if(n==0 || n==1|| n==2)
+    static int printTribRec(int n) {
+        if (n == 0 || n == 1 || n == 2)
             return 0;
-        if(n==3)
+        if (n == 3)
             return 1;
         else
-            return printTribRec(n-1) + printTribRec(n-2) + printTribRec(n-3);
+            return printTribRec(n - 1) + printTribRec(n - 2) + printTribRec(n - 3);
     }
 }
